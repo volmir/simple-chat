5bf46c9a42efad6399b1ea3e69ea8e1e5ed2d56f
Vladimir Prokhnenko <volmir@ukr.net>
2019-12-06 22:23:58 +0200

    Add project files

--- web/index.php
+++ web/index.php
@@ -5,8 +5,8 @@ session_start();
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$loader = require(__DIR__ . '/vendor/autoload.php');
-$config = require(__DIR__ . '/config/config.php');
+$loader = require(__DIR__ . '/../vendor/autoload.php');
+$config = require(__DIR__ . '/../config/config.php');
 
 $appication = new \App\Core\Application();
 $appication->run($config);
